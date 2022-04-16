90addcf9cc8c54252bb0d20b0f322c5650694cdb
David <jamcarbon@gmail.com>
2022-04-16 12:45:53 +1200

    update

--- terraform/7-nodes.tf
+++ terraform/7-nodes.tf
@@ -46,7 +46,7 @@ resource "aws_eks_node_group" "general" {
   instance_types = ["c6g.medium"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 50
     min_size     = 1
   }
