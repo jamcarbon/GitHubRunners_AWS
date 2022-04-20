c42e29dc4ced8fa875d9a4633da40928e6d61a5a
David Montenegro <jamcarbon@users.noreply.github.com>
2022-04-21 11:17:18 +1200

    update

--- terraform/8-Cloudwatch_alarms.tf
+++ terraform/8-Cloudwatch_alarms.tf
@@ -1,25 +1,44 @@
-resource "aws_autoscaling_policy" "bat" {
-  name                   = "foobar3-terraform-test"
+resource "aws_autoscaling_group" "autoscalegroup" {
+  name                 = "autoscalegroup"
+  min_size             = 1
+  max_size             = 4
+  health_check_grace_period = 300
+  health_check_type         = "EC2"
+  desired_capacity     = 1
+  default_cooldown     = 120
+  vpc_zone_identifier  = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id]
+
+  role_arn                = aws_iam_role.nodes.arn
+
+  tag {
+    key                 = "Name"
+    value               = "AutoscaleGroup"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_policy" "asp" {
+  name                   = "autoscalepolicy"
   scaling_adjustment     = 2
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.bar.name
+  autoscaling_group_name = aws_autoscaling_group.autoscalegroup
 }
 
-resource "aws_cloudwatch_metric_alarm" "bat" {
-  alarm_name          = "terraform-test-foobar5"
+resource "aws_cloudwatch_metric_alarm" "95" {
+  alarm_name          = "CPUautoscale95"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "60"
+  period              = "120"
   statistic           = "Average"
-  threshold           = "80"
+  threshold           = "95"
 
   dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.bar.name
+    AutoScalingGroupName = aws_autoscaling_group.autoscalegroup
   }
 
-  alarm_description = "This metric monitors ec2 cpu utilization"
-  alarm_actions     = [aws_autoscaling_policy.bat.arn]
+  alarm_description = "Autoscale when more than 95% CPU"
+  alarm_actions     = [aws_autoscaling_policy.95.autoscalepolicy]
 }
