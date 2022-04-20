3a0fd2dfa1c7ec118414c7320b73b67c534bbb68
David <jamcarbon@gmail.com>
2022-04-21 08:25:25 +1200

    update

---
+++ terraform/8-Cloudwatch_alarms.tf
@@ -0,0 +1,25 @@
+resource "aws_autoscaling_policy" "bat" {
+  name                   = "foobar3-terraform-test"
+  scaling_adjustment     = 2
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.bar.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "bat" {
+  alarm_name          = "terraform-test-foobar5"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "80"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.bar.name
+  }
+
+  alarm_description = "This metric monitors ec2 cpu utilization"
+  alarm_actions     = [aws_autoscaling_policy.bat.arn]
+}
