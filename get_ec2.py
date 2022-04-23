e3f1c4798bd6b94dcf6f39d28e1734a9e7121c91
David <jamcarbon@gmail.com>
2022-04-23 20:13:15 +1200

    update

--- get_ec2.py
+++ get_ec2.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python
 
 import sys, getopt
+import boto3
 
 def main(argv):
-   inputfile = ''
-   outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print ('Error, please run: \n'
-             'get_ec2.py -running (to get running intances)\n'
-             'get_ec2.py -all (to get all intances)')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print ('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("runninginstances"):
-         inputfile = arg
-      elif opt in ("all"):
-         outputfile = arg
-   print ('Input file is "', inputfile)
-   print ('Output file is "', outputfile)
+   # !/usr/bin/python
+
+   ec2 = boto3.resource('ec2')
+   client = boto3.client('autoscaling')
+
+   response = client.describe_auto_scaling_groups(MaxRecords=1)
+   autoscalingfulldetails = response['AutoScalingGroups']
+   autoscalegroups = autoscalingfulldetails[0]
+   myautoscalinggroup = autoscalegroups['AutoScalingGroupName']
+   currentdesiredcapacity = autoscalegroups['DesiredCapacity']
+
+   print("There are currently ", currentdesiredcapacity, " instances schedule to start or running:", "\n")
+
+   instances = ec2.instances.filter(
+      Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+   for instance in instances:
+      print(instance.id, instance.instance_type, "\n")
+
+   awscost = currentdesiredcapacity * 0.5
+
+   print("The current cost for all resources deployed in AWS is aproximately: ", "$", awscost, "per hour.")
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
--- terraform/8-Cloudwatch_alarms.tf
+++ terraform/8-Cloudwatch_alarms.tf
@@ -1,12 +1,5 @@
 # Resource: aws_autoscaling_policy
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_policy
-resource "aws_autoscaling_policy" "upCPU95" {
-  name                   = "UpscaleCPU95"
-  scaling_adjustment     = 3
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 240
-  autoscaling_group_name = aws_eks_node_group.noderunners.resources[0].autoscaling_groups[0].name
-}
 
 resource "aws_autoscaling_policy" "downCPU15" {
   name                   = "DownscaleCPU15"
@@ -16,33 +9,13 @@ resource "aws_autoscaling_policy" "downCPU15" {
   autoscaling_group_name = aws_eks_node_group.noderunners.resources[0].autoscaling_groups[0].name
 }
 
-# Resource: aws_cloudwatch_metric_alarm
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_metric_alarm
-resource "aws_cloudwatch_metric_alarm" "CPUauto95" {
-  alarm_name          = "CPUautoscale95"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "240"
-  statistic           = "Average"
-  threshold           = "95"
-
-  dimensions = {
-    AutoScalingGroupName = aws_eks_node_group.noderunners.resources[0].autoscaling_groups[0].name
-  }
-
-  alarm_description = "Autoscale when more than 95% CPU"
-  alarm_actions     = [aws_autoscaling_policy.upCPU95.arn]
-}
-
 resource "aws_cloudwatch_metric_alarm" "CPUauto15" {
   alarm_name          = "CPUautoscale15"
   comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "2"
+  evaluation_periods  = "8"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "1800"
+  period              = "300"
   statistic           = "Average"
   threshold           = "15"
 
