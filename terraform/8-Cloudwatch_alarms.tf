e487ea83d387fe4bfec98cf44c73651a450804df
David <jamcarbon@gmail.com>
2022-04-23 20:14:12 +1200

    update

--- terraform/8-Cloudwatch_alarms.tf
+++ terraform/8-Cloudwatch_alarms.tf
@@ -1,15 +1,15 @@
 # Resource: aws_autoscaling_policy
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/autoscaling_policy
 
-resource "aws_autoscaling_policy" "downCPU15" {
-  name                   = "DownscaleCPU15"
+resource "aws_autoscaling_policy" "downCPU5" {
+  name                   = "DownscaleCPU5"
   scaling_adjustment     = -3
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 3600
   autoscaling_group_name = aws_eks_node_group.noderunners.resources[0].autoscaling_groups[0].name
 }
 
-resource "aws_cloudwatch_metric_alarm" "CPUauto15" {
+resource "aws_cloudwatch_metric_alarm" "CPUauto5" {
   alarm_name          = "CPUautoscale15"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "8"
@@ -17,12 +17,12 @@ resource "aws_cloudwatch_metric_alarm" "CPUauto15" {
   namespace           = "AWS/EC2"
   period              = "300"
   statistic           = "Average"
-  threshold           = "15"
+  threshold           = "5"
 
   dimensions = {
     AutoScalingGroupName = aws_eks_node_group.noderunners.resources[0].autoscaling_groups[0].name
   }
 
-  alarm_description = "Downscale when less than 15% CPU"
-  alarm_actions     = [aws_autoscaling_policy.downCPU15.arn]
+  alarm_description = "Downscale when less than 5% CPU"
+  alarm_actions     = [aws_autoscaling_policy.downCPU5.arn]
 }
