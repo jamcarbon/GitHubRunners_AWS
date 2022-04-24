f7a1cf1c32b4c0952b3cc66696a4d47892790053
David <jamcarbon@gmail.com>
2022-04-25 10:14:38 +1200

    update

--- terraform/0-provider.tf
+++ terraform/0-provider.tf
@@ -9,13 +9,5 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.2.0"
-    }
   }
 }
