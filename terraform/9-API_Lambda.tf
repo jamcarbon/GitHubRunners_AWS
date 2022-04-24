97acef90df15d29e9a639584e92a5de896b5bf85
David <jamcarbon@gmail.com>
2022-04-24 22:48:39 +1200

    update

--- terraform/9-API_Lambda.tf
+++ terraform/9-API_Lambda.tf
@@ -3,7 +3,7 @@
 
 resource "aws_iam_policy" "LambdaAutoscaling" {
   name = "LambdaAutoscaling"
-
+  arn = "arn:aws:iam::123456789012:policy/UsersManageOwnCredentials"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
